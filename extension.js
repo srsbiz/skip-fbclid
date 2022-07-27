88b4d942b749f2dea4f8dde1b4177901b8acce55
Radosław Kowalewski <radoslaw.kowalewski@gt.pl>
2022-07-27 17:23:35 +0200

    Initial commit

---
+++ extension.js
@@ -0,0 +1,21 @@
+chrome.contextMenus.create({title: 'Open without tracking', id: 'skip-fbclid', contexts: ['link']});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    const fbLinker = 'l.facebook.com/l.php?u='
+    let url = info.linkUrl,
+        linkerPos = url.lastIndexOf(fbLinker),
+        fbClidPos = -1
+    ;
+    if (linkerPos > -1) {
+        url = unescape(url.substring(linkerPos + fbLinker.length));
+    }
+    if (
+        -1 < (fbClidPos = url.lastIndexOf('?fbclid='))
+        ||
+        -1 < (fbClidPos = url.lastIndexOf('&fbclid='))
+    ) {
+        url = url.substring(0, fbClidPos);
+    }
+    console.log(url);
+    chrome.tabs.create({url: url, active: true});
+})
