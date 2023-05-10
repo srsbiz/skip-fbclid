938241fd05518b1241757d98b63f559c59723656
Radosław Kowalewski <rk@srsbiz.pl>
2023-05-11 00:12:39 +0200

    support selection

--- extension/extension.js
+++ extension/extension.js
@@ -1,14 +1,21 @@
-chrome.contextMenus.create({title: 'Open incognito without tracking', id: 'skip-fbclid', contexts: ['link']});
+chrome.contextMenus.create({
+    title: 'Open incognito without tracking',
+    id: 'skip-fbclid',
+    contexts: [
+        'link',
+        'selection'
+    ]
+});
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     const fbLinker = 'l.facebook.com/l.php?u='
-    let url = info.linkUrl,
+    let url = info.linkUrl || info.selectionText,
         linkerPos = url.lastIndexOf(fbLinker),
         fbClidPos = -1,
         tabOptions = { active: true }
     ;
     if (linkerPos > -1) {
-        url = unescape(url.substring(linkerPos + fbLinker.length));
+        url = decodeURIComponent(url.substring(linkerPos + fbLinker.length));
     }
     if (
         -1 < (fbClidPos = url.lastIndexOf('?fbclid='))
@@ -16,6 +23,13 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
         -1 < (fbClidPos = url.lastIndexOf('&fbclid='))
     ) {
         url = url.substring(0, fbClidPos);
+    }
+    if (url.lastIndexOf('https://') !== 0 && url.lastIndexOf('http://') !== 0) {
+        if (url.lastIndexOf('.') == -1 || url.lastIndexOf(' ') >= 0) {
+            url = 'https://www.google.com/search?q=' + encodeURIComponent(url);
+        } else {
+            url = 'http://' + url;
+        }
     }
     tabOptions.url = url;
     chrome.windows.getAll({ populate: true, windowTypes: ["normal"] }, function(allWindows){
